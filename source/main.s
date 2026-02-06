aca2b064fe8512156af304ac498700a2d19e0161
Cauan Weber <cauandeveloper101@gmail.com>
2026-02-06 01:32:26 -0300

    feat: implement syscall-based menu dispatcher (ELF64)

--- source/main.s
+++ source/main.s
@@ -23,8 +23,24 @@ Start:
     SYSCALL
 
     MOV     BL, [INPUT_Buffer]
-    CMP     BL, 49     ; ASCII ('1')
+
+    CMP     BL, 49      ; ASCII ('1')
     JE      OPTION_Sum
+
+    CMP     BL, 50      ; ASCII ('2')
+    JE      OPTION_Subtract
+
+    CMP     BL, 51      ; ASCII ('3')
+    JE      OPTION_Multiply
+
+    CMP     BL, 52      ; ASCII ('4')
+    JE      OPTION_Divisor
+
+    MOV     RAX, 1
+    MOV     RDI, 1
+    MOV     RSI, TEXT_Invalid_Option
+    MOV     RDX, TEXT_Invalid_Option_LENGTH
+    SYSCALL
 
 Exit:
     MOV     RAX, 60
@@ -40,6 +56,33 @@ OPTION_Sum:
 
     JMP     Exit
 
+OPTION_Subtract:
+    MOV     RAX, 1
+    MOV     RDI, 1
+    MOV     RSI, TEXT_Subtract_Selected
+    MOV     RDX, TEXT_Subtract_Selected_LENGTH
+    SYSCALL
+
+    JMP     Exit
+
+OPTION_Multiply:
+    MOV     RAX, 1
+    MOV     RDI, 1
+    MOV     RSI, TEXT_Multiply_Selected
+    MOV     RDX, TEXT_Multiply_Selected_LENGTH
+    SYSCALL
+
+    JMP     Exit
+
+OPTION_Divisor:
+    MOV     RAX, 1
+    MOV     RDI, 1
+    MOV     RSI, TEXT_Divisor_Selected
+    MOV     RDX, TEXT_Divisor_Selected_LENGTH
+    SYSCALL
+
+    JMP     Exit
+
 SEGMENT READABLE WRITEABLE
 
     INPUT_Buffer_LENGTH = 2
@@ -48,14 +91,25 @@ SEGMENT READABLE WRITEABLE
     TEXT_Title DB "Bravion Calculator", 10
     TEXT_Title_LENGTH = $ - TEXT_Title
 
-    TEXT_Main_Menu DB 10
-                   DB "Choose operation:", 10
-                   DB "1) Sum", 10
-                   DB "2) Subtract", 10
-                   DB "3) Multiply", 10
-                   DB "4) Divisor", 10
+    TEXT_Main_Menu DB 10, "Choose operation:", 10
+                   DB     "1) Sum", 10
+                   DB     "2) Subtract", 10
+                   DB     "3) Multiply", 10
+                   DB     "4) Divisor", 10
 
     TEXT_Main_Menu_LENGTH = $ - TEXT_Main_Menu
 
     TEXT_Sum_Selected DB "Sum selected", 10
     TEXT_Sum_Selected_LENGTH = $ - TEXT_Sum_Selected
+
+    TEXT_Subtract_Selected DB "Subtract selected", 10
+    TEXT_Subtract_Selected_LENGTH = $ - TEXT_Subtract_Selected
+
+    TEXT_Multiply_Selected DB "Multiply selected", 10
+    TEXT_Multiply_Selected_LENGTH = $ - TEXT_Multiply_Selected
+
+    TEXT_Divisor_Selected DB "Divisor selected", 10
+    TEXT_Divisor_Selected_LENGTH = $ - TEXT_Divisor_Selected
+
+    TEXT_Invalid_Option DB "Invalid option", 10
+    TEXT_Invalid_Option_LENGTH = $ - TEXT_Invalid_Option
